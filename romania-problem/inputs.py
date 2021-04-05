de251d6ef802dd2a2acfa099390810c677468674
finesaaa <indriatifiqey16@gmail.com>
2021-04-05 14:27:52 +0700

    update modular astar

---
+++ romania-problem/inputs.py
@@ -0,0 +1,26 @@
+class CityNode:
+    def __init__(self, city_name, dist):
+        self.city = str(city_name)
+        self.dist = int(dist)
+
+def get_romania_map():
+    romania = {}
+    file = open("romania.txt", 'r')
+    for line in file:
+        arg = line.split(',')
+        city1 = arg[0]
+        city2 = arg[1]
+        dist = int(arg[2])
+        romania.setdefault(city1, []).append(CityNode(city2, dist))
+        romania.setdefault(city2, []).append(CityNode(city1, dist))
+    return romania
+
+def make_heuristic_dict():
+    heuristic = {}
+    file = open("romania_sld.txt", 'r')
+    for line in file:
+        arg = line.split(',')
+        city = arg[0]
+        value = int(arg[1])
+        heuristic[city] = value
+    return heuristic
--- romania-problem/search_algorithms.py
+++ romania-problem/search_algorithms.py
@@ -1,5 +1,29 @@
 from priority_queue import PriorityQueue
 
+def dfs(src, dst, romania):
+    path = {}
+    distance = {}
+    pq = PriorityQueue()
+
+    pq.push(src, 0)
+    distance[src] = 0
+    path[src] = None
+    expandedlist = []
+
+    while (pq.is_empty() == False):
+        curr = pq.pop()
+        if (curr[1] == dst):
+            break
+        expandedlist.append(curr[1])
+        for new in romania[curr[1]]:
+            cost = int(curr[0]) + int(new.dist)
+            if (new.city not in distance):
+                distance[new.city] = cost
+                pq.push(new.city, cost)
+                path[new.city] = curr[1]
+    
+    print_path(src, dst, path, distance, expandedlist, "DFS")
+
 def greedy_bfs(src, dst, romania, heuristic):
     path = {}
     distance = {}
@@ -12,23 +36,51 @@ def greedy_bfs(src, dst, romania, heuristic):
 
     while (pq.is_empty() == False):
         curr = pq.pop()
-        expandedlist.append(curr)
+        expandedlist.append(curr[1])
 
-        if (curr == dst):
+        if (curr[1] == dst):
             break
 
-        for new in romania[curr]:
-            gcost = distance[curr] + int(new.dist)
+        for new in romania[curr[1]]:
+            gcost = distance[curr[1]] + int(new.dist)
 
             if (new.city not in distance):
                 distance[new.city] = gcost
                 fcost = heuristic[new.city]
                 pq.push(new.city, fcost)
-                path[new.city] = curr
+                path[new.city] = curr[1]
     
-    print_path(src, dst, path, distance, expandedlist)
+    print_path(src, dst, path, distance, expandedlist, "Greedy BFS")
 
-def print_path(src, dst, path, distance, expandedlist):
+def astar(src, dst, romania, heuristic):
+    path = {}
+    distance = {}
+    pq = PriorityQueue()
+
+    pq.push(src, 0)
+    distance[src] = 0
+    path[src] = None
+    expandedlist = []
+
+    while (pq.is_empty() == False):
+        curr = pq.pop()
+        expandedlist.append(curr[1])
+
+        if (curr[1] == dst):
+            break
+
+        for new in romania[curr[1]]:
+            gcost = distance[curr[1]] + int(new.dist)
+
+            if (new.city not in distance or gcost < distance[new.city]):
+                distance[new.city] = gcost
+                fcost = gcost + heuristic[new.city]
+                pq.push(new.city, fcost)
+                path[new.city] = curr[1]
+    
+    print_path(src, dst, path, distance, expandedlist, "A Star")
+
+def print_path(src, dst, path, distance, expandedlist, method):
     fixpath = []
     end = dst
 
@@ -37,7 +89,7 @@ def print_path(src, dst, path, distance, expandedlist):
         end = path[end]
     fixpath.append(src)
     fixpath.reverse()
-    print("Program algoritma Astar untuk masalah Romania\tArad => Bucharest")
+    print("Program algoritma " + str(method) + " untuk masalah Romania Arad => Bucharest")
     print("=======================================================")
     print("Kota yg mungkin dijelajah \t\t: " + str(expandedlist))
     print("Jumlah kemungkinan kota \t\t: " + str(len(expandedlist)))
