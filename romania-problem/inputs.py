1207f335761f22ff5ac3bd181a44f90ea2fc4e9b
maroqijalil <jalilpro31@gmail.com>
2021-04-06 20:14:38 +0700

    minor change

--- romania-problem/inputs.py
+++ romania-problem/inputs.py
@@ -1,26 +1,34 @@
-class CityNode:
+from typing import Dict, Any
+class City:
+
     def __init__(self, city_name, dist):
         self.city = str(city_name)
         self.dist = int(dist)
 
-def get_romania_map():
+
+def get_romania_map() -> Dict[str, Any]:
     romania = {}
     file = open("romania.txt", 'r')
+
     for line in file:
         arg = line.split(',')
         city1 = arg[0]
         city2 = arg[1]
         dist = int(arg[2])
-        romania.setdefault(city1, []).append(CityNode(city2, dist))
-        romania.setdefault(city2, []).append(CityNode(city1, dist))
+        romania.setdefault(city1, []).append(City(city2, dist))
+        romania.setdefault(city2, []).append(City(city1, dist))
+
     return romania
 
-def get_romania_heuristic():
+
+def get_romania_heuristic() -> Dict[str, int]:
     heuristic = {}
     file = open("romania_sld.txt", 'r')
+
     for line in file:
         arg = line.split(',')
         city = arg[0]
         value = int(arg[1])
         heuristic[city] = value
+    
     return heuristic
